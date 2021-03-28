a3768fed4d3e4ea34e6e331e15b45d6563b75338
野口寛生 <ilmo.an.stxzz@gmail.com>
2021-03-28 09:31:27 +0900

    モデルのテスト実装

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -21,8 +21,8 @@
 #
 FactoryBot.define do
   factory :comment do
-    body { "MyText" }
-    user { nil }
-    article { nil }
+    body { Faker::String.random(length: 3..140) }
+    user
+    article
   end
 end
