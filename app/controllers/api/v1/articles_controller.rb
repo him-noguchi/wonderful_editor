1a0d415342d9ffb532a82ea28f177f94b8441b97
野口寛生 <ilmo.an.stxzz@gmail.com>
2021-04-09 05:52:15 +0900

    記事作成とそのテストの実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class ArticlesController < BaseApiController
+
     def index
       articles = Article.order(created_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
@@ -9,5 +10,24 @@ module Api::V1
       article = Article.find(params[:id])
       render json: article, each_serializer: Api::V1::ArticleSerializer
     end
+
+    def create
+      @article = Article.new(
+        title: article_params[:title],
+        body: article_params[:body],
+         user_id: current_user.id
+        )
+
+      @article.save!
+
+      render json: @article, each_serializer: Api::V1::ArticleSerializer
+    end
+
+    private
+
+      def article_params
+        params.require(:article).permit(:title, :body).merge(user_id: current_user.id)
+      end
+
   end
 end
