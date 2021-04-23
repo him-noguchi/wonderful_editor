a2691a121c1d232abb9c7140789d52e0a5c807f6
野口寛生 <ilmo.an.stxzz@gmail.com>
2021-04-23 23:08:53 +0900

    rubocopにより一部修正

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
   describe "POST　/api/v1/auth" do
-    subject { post(api_v1_user_registration_path, params: params)}
+    subject { post(api_v1_user_registration_path, params: params) }
 
     context "適切なパラメータを送ったとき" do
-      let(:params) { attributes_for(:user)}
+      let(:params) { attributes_for(:user) }
 
       it "新規登録ができる" do
-        expect{ subject }.to change{ User.count }.by(1)
+        expect { subject }.to change { User.count }.by(1)
         res = JSON.parse(response.body)
         expect(response).to have_http_status(:ok)
-        expect(res["data"]["email"]).to eq (User.last.email)
+        expect(res["data"]["email"]).to eq(User.last.email)
       end
 
-      fit "header情報を取得できる" do
+      it "header情報を取得できる" do
         subject
         header = response.header
         expect(header["access-token"]).to be_present
@@ -26,36 +26,35 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
     end
 
     context "name が空のとき" do
-      let(:params) { attributes_for(:user, name: nil)}
+      let(:params) { attributes_for(:user, name: nil) }
 
       it "新規登録に失敗する" do
         expect { subject }.to change { User.count }.by(0)
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
         res = JSON.parse(response.body)
         expect(res["errors"]["name"]).to eq ["can't be blank"]
       end
     end
 
     context "email が空のとき" do
-      let(:params) { attributes_for(:user, email: nil)}
+      let(:params) { attributes_for(:user, email: nil) }
 
       it "新規登録に失敗する" do
         expect { subject }.to change { User.count }.by(0)
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
         res = JSON.parse(response.body)
         expect(res["errors"]["email"]).to eq ["can't be blank"]
       end
     end
 
     context "password が空のとき" do
-      let(:params) { attributes_for(:user, password: nil)}
+      let(:params) { attributes_for(:user, password: nil) }
 
       it "新規登録に失敗する" do
         expect { subject }.to change { User.count }.by(0)
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
         res = JSON.parse(response.body)
         expect(res["errors"]["password"]).to eq ["can't be blank"]
-
       end
     end
   end
